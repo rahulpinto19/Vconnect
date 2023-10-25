9d154e9bd8179ceacc831c0622930b775166fb6c
mohit-8692 <mohitgupta8692@gmail.com>
2023-10-25 12:11:58 +0530

    updated code

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,16 +5,16 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="Get latest updates and be connected with world" text="Here,we will mentor to the cuh students and get them ready for joining the corporate world ." />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
       <p>Explore the platform</p>
     </div>
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Hackathon" text="Participate in the contest and get ready for embracing your coding skills.We are going to share hackathone which will be held in Cuh as well as Hackathons organised by some recognized platforms." />
+      <Feature title="Job Opportunity" text=" Here we will provide some recent job opening which anyone can apply through off-campus.We have done a lot of research for providing relevant job links." />
+      <Feature title="Workshop" text="Experience can let you achieve your success with some easy steps.So join this workshop and listen to the knowledge and experience of some profound experts in industry as well as in education." />
     </div>
   </div>
 );
